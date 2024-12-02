189fcc43db6ae549066f188637a6e9286697dba9
kangsun1 <sunkang1224@naver.com>
2024-12-02 16:43:12 +0900

    메인 페이지 제작 2(헤더 부분 추가, MAIN1 부분 제작 중)

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -1,29 +1,35 @@
-<<<<<<< HEAD
-/*메인페이지*/
+//메인페이지
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import './MainPage.css';
+import './MainPage.css'; //
 
 function MainPage() {
     return (
+        
+        <> {/* 여러 부모 요소를 사용하기 위함 */}
+        {/* 헤더 영역 */}
         <div>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                <Navbar.Brand id="title"><h1>Pentagon</h1></Navbar.Brand>
-                <Nav className = "side-nav">
-                    <Nav.Link>운동</Nav.Link>
-                    <Nav.Link>기록</Nav.Link>
-                    <Nav.Link>상담</Nav.Link>
-                    <Nav.Link>로그인</Nav.Link>
-                    <Nav.Link>회원가입</Nav.Link>
-                </Nav>
-                </Container>
-            </Navbar>
+            <header className="header">
+                <Navbar className="navbar">
+                    <Container className="navbar-container">
+                        <Navbar.Brand style={{ color: 'white', marginLeft: '30px' }}><h1>Pentagon</h1></Navbar.Brand>
+                            <Nav className="nav-links" style={{ marginRight: '30px' }}>
+                                <Nav.Link className="side-navlink">운동</Nav.Link>
+                                <Nav.Link className="side-navlink">기록</Nav.Link>
+                                <Nav.Link className="side-navlink">상담</Nav.Link>
+                                <Nav.Link className="side-navlink">로그인</Nav.Link>
+                                <Nav.Link className="side-navlink">회원가입</Nav.Link>
+                            </Nav>
+                    </Container>
+                </Navbar>
+            </header>
         </div>
+        <div id="introduce-page">
+            <h1 className="mainpage_title">운동에 관한 모든 것, The Pentagon. 펜타곤은 각종 소요층이 요구하는 운동 정보와 본인의 의지로 이뤄낸 운동 레코드 서비스를 제공하는 사이트입니다.</h1>
+        </div>
+        </>
+        
     )
 }
 
 export default MainPage;
-=======
-/* 메인페이지 작업 시작 */
->>>>>>> ba48d62169a23e753ad03fdc86c30f197c12cf7a
