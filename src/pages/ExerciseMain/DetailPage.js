289c788e3d106c36f98c0bc2f16fba56af817eca
starble25 <starble25@gmail.com>
2024-12-02 17:22:32 +0900

    운동 상세페이지 시작

---
+++ src/pages/ExerciseMain/DetailPage.js
@@ -0,0 +1,29 @@
+import '../../assets/DetailPage.css'
+
+
+function DetailPage() {
+    
+
+    return (
+        <div className='mn-container'>
+            <div className='page-title'>
+                <h1>초급자 코스</h1>
+            </div>
+            <div className='contentWrap'>
+                <div className='contContainer'>
+                    <div className='contTitle'></div>
+                    <div className='contImg'></div>
+                    <div className='contText'></div>
+                </div>
+
+                <div className='contContainer'>
+                    <div className='contTitle'></div>
+                    <div className='contImg'></div>
+                    <div className='contText'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetailPage;
