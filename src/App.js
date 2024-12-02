96f236d8154d040be3304a90c164089956d7b210
PCJ-93 <pw2900@naver.com>
2024-12-02 16:18:38 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-
-
-import './App.css';
-
-import RecordMain from './pages/record/RecordMain';
-
-
-=======
 import './App.css'; // 스타일시트가 필요하다면 포함
 import RecordMain from './pages/RecordMain'; // RecordMain을 올바르게 임포트
->>>>>>> 1fc232004029eaf5594c45853b541d00f529c452
+
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <RecordMain/>
-=======
       <RecordMain /> {/* RecordMain 컴포넌트를 정상적으로 렌더링 */}
->>>>>>> 1fc232004029eaf5594c45853b541d00f529c452
     </>
   );
 }
