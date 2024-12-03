6529b987222638d3292a7b2931c7b88b9cf812c9
starble25 <starble25@gmail.com>
2024-12-03 17:14:42 +0900

    상세페이지 컴포넌트화 & 기능 추가

--- src/pages/ExerciseMain/DetailPage.js
+++ src/pages/ExerciseMain/DetailPage.js
@@ -1,21 +1,42 @@
 import '../../assets/DetailPage.css'
 import testimage from '../../assets/testimage.png'
 import ExDescription from './ExDescription';
+import ExContentBox from './ExContentBox';
 import { useState } from 'react';
-
+import DcData from './DcData';
 
 function DetailPage() {
-    // process.env.PUBLIC_URL
     let image = testimage;
 
     let [contentData, setContentData] = useState([
         {
-            name: '운동이름',
-            imageURL: '',
+            id: '001',
+            name: '푸시업',
+            imageURL: image,
             content: '설명',
             flag: true
+        },
+
+        {
+            id: '002',
+            name: '싯업',
+            imageURL: image,
+            content: '설명',
+            flag: false
         }
     ]);
+    
+        // 운동가이드 설명칸 데이터
+        // 해당 난이도 객체들만 dcData로 복사
+        let [dcData, setDcData] = useState(DcData.filter(DcData => DcData.difficulty == 'easy'));
+        
+        // contentData.flag : true인 id값 가져오기
+        let [idValue, setidValue] = useState(
+            contentData.filter(item => item.flag === true).map(item => item.id)
+        );
+        const getId = ()=>{
+            setidValue(contentData.filter(item => item.flag === true).map(item => item.id));
+        }
 
     return (
         <>
@@ -24,34 +45,33 @@ function DetailPage() {
                     <h1>초급자 코스</h1>
                 </div>
                 <div className='contentWrap'>
-                    <div className='contContainer' onClick={()=>{
-                        let temp = [...contentData];
-                        temp[0].flag = temp[0].flag ? false : true;
-                        setContentData(temp);
-                        console.log(contentData[0].flag);
-
-                        // ++ 다른 index false로 바꾸기
-                    }}>
-                        <div className='contTitle'>{contentData[0].name}</div>
-                        <img
-                            className='contImg'
-                            src={image}
-                        ></img>
-                        <div className='contText'>{contentData[0].content}</div>
-                    </div>
-
-                    <div className='contContainer'>
-                        <div className='contTitle'>운동이름</div>
-                        <img
-                            className='contImg'
-                            src={image}
-                        ></img>
-                        <div className='contText'>설명</div>
-                    </div>
+                    {/* 운동 컴포넌트 */}
+                    {
+                        contentData.map((item, index)=>{
+                            return (
+                                <ExContentBox
+                                    contentData={contentData}
+                                    setContentData={setContentData}
+                                    index={index}
+                                    getId={getId}
+                                />
+                            )
+                        })
+                    }
                 </div>
 
+                {/* description 컴포넌트 */}
                 {
-                    <ExDescription />
+                    dcData.map((item, index)=>{
+                        return (
+                            item.id == idValue ?
+                            <ExDescription
+                                dcData={dcData}
+                                setDcData={setDcData}
+                                index={index}
+                            /> : null
+                        )
+                    })
                 }
             </div>
 
---
+++ src/pages/ExerciseMain/ExContentBox.js
@@ -0,0 +1,28 @@
+
+function ExContentBox( {contentData, setContentData, index, getId} ) {
+
+    return (
+        <>
+            <div className='contContainer' onClick={() => {
+                // 버튼 누른 인덱스 flag = true
+                // 다른 인덱스들은 모두 false로 바꾸기
+                let temp = [...contentData];
+                temp.map((item, idx)=>{
+                    temp[idx].flag = false;
+                })
+                temp[index].flag = true;
+                getId();
+                setContentData(temp);
+            }}>
+                <div className='contTitle'>{contentData[index].name}</div>
+                <img
+                    className='contImg'
+                    src={contentData[index].imageURL}
+                ></img>
+                <div className='contText'>{contentData[index].content}</div>
+            </div>
+        </>
+    )
+}
+
+export default ExContentBox;
