ec327a6748feb6b42cbadfbb34e5eae1fc55e77d
kangsun1 <sunkang1224@naver.com>
2024-12-04 09:28:50 +0900

    maintitle image에서 video방식으로 변경

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -12,8 +12,8 @@ function MainPage() {
             <header className="header">
                 <Navbar className="navbar">
                     <Container className="navbar-container">
-                        <Navbar.Brand style={{ color: 'white', marginLeft: '30px' }}><h1>Pentagon</h1></Navbar.Brand>
-                            <Nav className="nav-links" style={{ marginRight: '30px' }}>
+                        <Navbar.Brand style={{ color: 'white', marginLeft: '50px' }}><h1>Pentagon</h1></Navbar.Brand>
+                            <Nav className="nav-links" style={{ marginRight: '50px' }}>
                                 <Nav.Link className="side-navlink">운동</Nav.Link>
                                 <Nav.Link className="side-navlink">기록</Nav.Link>
                                 <Nav.Link className="side-navlink">상담</Nav.Link>
@@ -25,10 +25,19 @@ function MainPage() {
             </header>
         </div>
         <div id="introduce-page">
-            <div className="background"></div>
-            <h1 className="mainpage_title">
-            운동에 관한 모든 것, The Pentagon. 펜타곤은 각종 소요층이 요구하는 운동 정보와 본인의 의지로 이뤄낸 운동 레코드 서비스를 제공하는 사이트입니다.
-            </h1>
+            <div className="background">
+                <video autoPlay loop muted playsInline>
+                    <source src='../../assets/video/maintitle.mp4' type='video/mp4'/>
+                </video>
+            </div>
+            <div className="title-container">
+                <h1 className="mainpage_title">
+                운동에 관한 모든 것, The Pentagon.
+                </h1>
+                <h2 className="mainpage_title2">
+                펜타곤은 각종 소요층이 요구하는 운동 정보와 운동 레코드 서비스를 제공합니다. 펜타곤과 함께 당신의 새로운 하루들을 시작해보세요.
+                </h2>
+            </div>
         </div>
         </>
         
