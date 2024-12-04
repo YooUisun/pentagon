360f0fc60689d6ecc468df6bc37ade97cbd8b13c
starble25 <starble25@gmail.com>
2024-12-04 17:12:22 +0900

    파일경로, css수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import DetailPage01 from './pages/ExerciseMain/DetailPage01';
-import DetailPage02 from './pages/ExerciseMain/DetailPage02';
-import DetailPage03 from './pages/ExerciseMain/DetailPage03';
+import DetailPage01 from './pages/exerciseMain/DetailPage01';
+import DetailPage02 from './pages/exerciseMain/DetailPage02';
+import DetailPage03 from './pages/exerciseMain/DetailPage03';
 
 function App() {
   return (
