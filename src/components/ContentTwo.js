5084fa563df97a9193e26192da4706d2ead90ca8
kangsun1 <sunkang1224@naver.com>
2024-12-04 16:45:56 +0900

    두번째 콘텐츠 컴포넌트화

---
+++ src/components/ContentTwo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import './ContentTwo.css';
+import title2 from '../../src/assets/image/page2title.jpg';
+
+function ContentTwo() {
+    return (
+        <Container id="introduce-page2">
+        <div className="content2">
+            <h1 className="page2-title">
+                운동 프로그램
+            </h1>
+            <p className="page2-description">
+                과학적인 기반의 통합 개인화된 훈련 프로그램을 안내하며, 각 코스별로 웰빙, 신체 및 스포츠 수행 목표를 충족하는 프로그램을 설계하여 소개해드립니다.
+            </p>
+            <p className="page2-description"> 
+                초급, 중급, 고급 프로그램 세분화된 운동 프로그램등을 통해 자신을 몸과 정신을 기쁘게 하는 경험을 누려보세요.
+            </p>
+                <button className="learn-more-button">자세히 보기</button> {/* 버튼 누르면 운동 메인 페이지로 */}
+            </div>
+            <div className="logoimage1">
+                <img src={title2} alt="Logoimage" style={{ width: '400px', height: '450px' }}/>
+            </div>
+        </Container>
+    )
+}
+export default ContentTwo;
--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './MainPage.css';
-import title2 from '../../assets/image/page2title.jpg';
 import Header from '../../components/Header'; // Header 컴포넌트 import.
 import Footer from '../../components/Footer'; // Footer 컴포넌트 import.
 import ContentOne from '../../components/ContentOne'; // Content1 컴포넌트 import.
+import ContentTwo from '../../components/ContentTwo'; // Content2 컴포넌트 import.
 
 
 function MainPage() {
@@ -15,24 +15,7 @@ function MainPage() {
         <Header/>;
         {/* 비디오 배경이 포함된 콘텐츠 영역 */}
         <ContentOne/>;
-        <Container id="introduce-page2">
-            <div className="content2">
-                <h1 className="page2-title">
-                    운동 프로그램
-                </h1>
-                <p className="page2-description">
-                    과학적인 기반의 통합 개인화된 훈련 프로그램을 안내하며, 각 코스별로 웰빙, 신체 및 스포츠 수행 목표를 충족하는 프로그램을 설계하여 소개해드립니다.
-                </p>
-                <p className="page2-description"> 
-                    초급, 중급, 고급 프로그램 세분화된 운동 프로그램등을 통해 자신을 몸과 정신을 기쁘게 하는 경험을 누려보세요.
-                </p>
-                <button className="learn-more-button">자세히 보기</button> {/* 버튼 누르면 운동 메인 페이지로 */}
-            </div>
-            <div className="logoimage1">
-                <img src={title2} alt="Logoimage" style={{ width: '400px', height: '450px' }}/>
-            </div>
-            
-        </Container>
+        <ContentTwo/>;
         <Container id="introduce-page3">
             <h1>
                 기록 메인
