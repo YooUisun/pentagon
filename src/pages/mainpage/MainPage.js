58df6fc9db39efd14ce4b42ced16ff21dacc9cff
kangsun1 <sunkang1224@naver.com>
2024-12-04 17:16:28 +0900

    스크롤 버튼 추가, 오타 수정

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -2,20 +2,33 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './MainPage.css';
-import Header from '../../components/Header' // Footer 컴포넌트 import.
-import Footer from '../../components/Footer' // Footer 컴포넌트 import.
-import ContentOne from '../../components/ContentOne' // Footer 컴포넌트 import.
-import ContentTwo from '../../components/ContentTwo' // Footer 컴포넌트 import.
+import Header from '../../components/Header'; // Footer 컴포넌트 import.
+import Footer from '../../components/Footer'; // Footer 컴포넌트 import.
+import ContentOne from '../../components/ContentOne'; // Footer 컴포넌트 import.
+import ContentTwo from '../../components/ContentTwo'; // Footer 컴포넌트 import.
 
 
 function MainPage() {
+    const scrollToTop =()=>{
+        window.scrollTo({
+          top: 0, // Y 좌표 0으로 설정 (맨 위)
+          behavior: 'smooth', // 부드럽게 스크롤
+        });
+    };
+
     return (
         <> {/* 여러 부모 요소를 사용하기 위함 */}
         {/* 헤더 영역 */}
-        <Header/>;
+        <Header/>
+        <div>
+      {/* 스크롤 버튼 */}
+    <button className="buttonStyle" onClick={scrollToTop}>
+        ↑ Top
+    </button>
+    </div>
         {/* 비디오 배경이 포함된 콘텐츠 영역 */}
-        <ContentOne/>;
-        <ContentTwo/>;
+        <ContentOne/>
+        <ContentTwo/>
         <Container id="introduce-page3">
             <h1>
                 기록 메인
