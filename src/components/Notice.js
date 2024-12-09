c153edd35d0c7778063ff9cacfe8c1321c75148e
kangsun1 <sunkang1224@naver.com>
2024-12-09 17:29:23 +0900

    공지 사항 제작 시작

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -1,7 +1,16 @@
-function Notice(){
-    return(
-        <h1 style={{marginTop:'70px'}}>공지사항</h1>
-    )
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Notice.css';
+
+function Notice() {
+    return (
+        <div className="notice-container">
+            <h1 className="notice-title">공지사항</h1>
+            <Link to="/not-found-page" className="notice-item">펜타곤 웹 페이지 최종 개설</Link>
+            <Link to="/not-found-page" className="notice-item">펜타곤 웹 페이지 개인정보 처리방침 개정</Link>
+            <Link to="/not-found-page" className="notice-item">펜타곤 웹 페이지 개설</Link>
+        </div>
+    );
 };
 
 export default Notice;
