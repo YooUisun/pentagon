bb4715a6e3c340c898f5bb05a4c90427d25d025f
psm0419 <sm8804@naver.com>
2024-12-10 11:47:40 +0900

    css 수정중

--- src/pages/ExerciseMain/ExerciseMain.js
+++ src/pages/ExerciseMain/ExerciseMain.js
@@ -21,23 +21,23 @@ function ExerciseMain() {
                 {/* 기본 경로 '/'에서 ExerciseMain을 렌더링 */}
                 
                     <Row className="justify-content-center centered-row">
-                        <Card className="m-4" style={{ width: '22rem' }} >
+                        <Card className="m-4" style={{ width: '22rem'}} >
                             {/* <Card.Img variant="top" src={lowLevelImage} style={{ height: '220px', objectFit: 'cover' }} /> */}
                             <div style={{ position: 'relative', height: '220px', overflow: 'hidden' }}>
                                 <video
+                                    className='video'
                                     src={lowLevelImage}
-                                    type="video/mp4"
-                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                    type="video/mp4"                                    
                                     autoPlay
                                     loop
-                                    muted
+                                    muted                                
                                 />
                             </div>
                             <Card.Body>
-                                <Card.Title>초급 프로그램</Card.Title>
-                                <Card.Text>근력 및 근 지구력 향상을 통한 대근육 발달과정</Card.Text>
+                                <Card.Title style={{color:'white'}}>초급 프로그램</Card.Title>
+                                <Card.Text style={{color:'white'}}>근력 및 근 지구력 향상을 통한 대근육 발달과정</Card.Text>
                                 <Link to="/details1">
-                                    <Button variant="primary">자세히 알아보기</Button>
+                                    <Button className='buttonStyle2'>자세히 알아보기</Button>
                                 </Link>
                             </Card.Body>
                         </Card>
@@ -48,28 +48,28 @@ function ExerciseMain() {
                                 <video
                                     src={midLevelImage}
                                     type="video/mp4"
-                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                    className='video'
                                     autoPlay
                                     loop
                                     muted
                                 />
                             </div>
-                            <Card.Body>
+                            <Card.Body style={{color:'white'}}>
                                 <Card.Title>중급 프로그램</Card.Title>
                                 <Card.Text>프리웨이트 기구를 통한 근육 강화와 덤벨과 바벨을 통한 세부근육 발달과정</Card.Text>
                                 <Link to="/details2">
-                                    <Button variant="primary">자세히 알아보기</Button>
+                                    <Button className='buttonStyle2'>자세히 알아보기</Button>
                                 </Link>
                             </Card.Body>
                         </Card>
 
                         <Card className="m-4" style={{ width: '22rem' }} >
                             <Card.Img variant="top" src={highLevelImage} style={{ height: '220px', objectFit: 'cover' }} />
-                            <Card.Body>
-                                <Card.Title>고급 프로그램</Card.Title>
+                            <Card.Body style={{color:'white'}}>
+                                <Card.Title style={{color:'red'}}>고급 프로그램</Card.Title>
                                 <Card.Text>대,소 근육밸런스와 근육의 조화발달, 프리웨이트를 이용한 발달 과정</Card.Text>
                                 <Link to="/details3">
-                                    <Button variant="primary">자세히 알아보기</Button>
+                                    <Button className='buttonStyle2'>자세히 알아보기</Button>
                                 </Link>
                             </Card.Body>
                         </Card>
