08e4548da799b3a492eb8e9c382aff917b180568
kangsun1 <sunkang1224@naver.com>
2024-12-10 15:07:47 +0900

    링크 연결 오류 수정

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -6,7 +6,7 @@ function NotFoundPage() {
         <div className="nf-container">
             <h1 className="nf-header">404 not found</h1>
             <p className="nf-message">Oops! The page you are looking for does not exist.</p>
-            <a href="/Mainpage" className="nf-link">
+            <a href="/" className="nf-link">
             Go Back Home
             </a>
         </div>
