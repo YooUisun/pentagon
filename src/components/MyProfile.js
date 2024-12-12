1c022c9e7d74e4d1ba8a9d07b7f774f71d088c31
kangsun1 <sunkang1224@naver.com>
2024-12-12 12:05:08 +0900

    Modal창 완성2

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -9,7 +9,7 @@ function MyProfile() {
                 <h2 style={{ fontSize: '24px' }}>닉네임님, 환영합니다!</h2>
             </div>
             <div className="modal-content">
-                <p>내 프로필 Modal 구현중.</p>
+                <p>내 프로필 Modal 구현중...</p>
                 <p>이곳에 내 정보가 표시됩니다.</p>
             </div>
         </div>
